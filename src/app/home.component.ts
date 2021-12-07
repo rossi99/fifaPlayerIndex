e14eb9b224a28a436582ed4d0cb91747ae8da55a
Ross Currie <ross.currie@teamwork.com>
2021-12-07 18:19:11 +0000

    Home page update

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -8,11 +8,13 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   ronaldoImagePath: string;
   messiImagePath: string;
+  schweinsteigerImagePath: string;
   neymarImagePath: string;
 
   constructor() {
     this.ronaldoImagePath = 'assets/images/home-img/ronaldo.png'
     this.messiImagePath = 'assets/images/home-img/messi.png'
+    this.schweinsteigerImagePath = 'assets/images/home-img/schweinsteiger.png'
     this.neymarImagePath = 'assets/images/home-img/neymar.png'
   }
 }
