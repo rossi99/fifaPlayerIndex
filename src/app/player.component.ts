abebe086bac5f6941cc1dd0151bc3956ba14cb98
Ross Currie <ross.currie@teamwork.com>
2021-12-09 19:52:39 +0000

    Updating routes for pages and testing review images

--- src/app/player.component.ts
+++ src/app/player.component.ts
@@ -9,19 +9,6 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent {
-  // string vars to hold image path
-  guy1 = 'assets/images/avatars/guy1.png'
-  guy2 = 'assets/images/avatars/guy2.png'
-  girl1 = 'assets/images/avatars/girl1.png'
-  girl2 = 'assets/images/avatars/girl2.png'
-
-  // Create a list to hold reviewer images
-  reviewer_img = [this.guy1, this.guy2, this.girl1, this.girl2]
-
-  // get random image for review
-  random_img = Math.floor(Math.random() * this.reviewer_img.length)
-  selected_img = this.reviewer_img[this.random_img]
-
   constructor(public webService: WebService, private route: ActivatedRoute, private formBuilder: FormBuilder) {}
 
   reviewForm: any;
@@ -35,6 +22,8 @@ export class PlayerComponent {
 
     this.player_list = this.webService.getPlayer(this.route.snapshot.params['id']);
     this.reviews = this.webService.getReviews(this.route.snapshot.params['id']);
+
+    console.log(this.reviews.username)
   }
 
   onSubmit() {
@@ -59,4 +48,17 @@ export class PlayerComponent {
 
   player_list: any = [];
   reviews: any = [];
+
+  // string vars to hold image path
+  guy1 = 'assets/images/avatars/guy1.png'
+  guy2 = 'assets/images/avatars/guy2.png'
+  girl1 = 'assets/images/avatars/girl1.png'
+  girl2 = 'assets/images/avatars/girl2.png'
+
+  // Create a list to hold reviewer images
+  reviewer_img = [this.guy1, this.guy2, this.girl1, this.girl2]
+
+  // get random image for review
+  random_img = Math.floor(Math.random() * this.reviewer_img.length)
+  selected_img = this.reviewer_img[this.random_img]
 }
