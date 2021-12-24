392eb6c8cc6f5915e20f0480f150099e776297cf
Ross Currie <ross.currie@teamwork.com>
2021-12-24 17:24:35 +0000

    Nav bar with login and out working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
+// Angular Imports
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AuthModule } from '@auth0/auth0-angular';
 
+// Custom Imports
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players.component';
 import { PlayerComponent } from './player.component';
@@ -8,12 +14,7 @@ import { HomeComponent } from './home.component';
 import { SkillComponent } from './skill.component';
 import { LoyalComponent } from './loyal.component';
 import { CreatePlayerComponent } from './create.component';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { WebService } from './web.service';
-import { HttpClientModule } from '@angular/common/http';
-
-import { RouterModule } from '@angular/router';
 
 var routes: any = [
   {
@@ -47,7 +48,11 @@ var routes: any = [
     AppComponent, PlayersComponent, HomeComponent, PlayerComponent, SkillComponent, LoyalComponent, CreatePlayerComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, RouterModule.forRoot(routes), ReactiveFormsModule
+    BrowserModule, HttpClientModule, RouterModule.forRoot(routes), ReactiveFormsModule,
+    AuthModule.forRoot({
+      domain: 'dev--jll02il.us.auth0.com',
+      clientId: 'ZrG6YWJINFIbxakggQ9lVDhQy6NAJapz'
+    })
   ],
   providers: [WebService],
   bootstrap: [AppComponent]
